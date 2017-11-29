4d778bdba46580529500a580ef2f2d989371c2d5
natschub <natschub@gmail.com>
2017-11-29 11:11:32 -0700

    Update best_states.sql

--- exercise_1/investigations/best_states/best_states.sql
+++ exercise_1/investigations/best_states/best_states.sql
@@ -1,4 +1,4 @@
-tate, count(provider_id), avg(score) as score, sum(score) as sum_score, variance(score)
+select state, count(provider_id), avg(score) as score, sum(score) as sum_score, variance(score)
 from eff_care_2
 group by state
 order by score DESC
